c9b5725533f31f266e7ab3ff20dc87a4d0c24b81
asp2131 <akinpds@gmail.com>
2023-10-23 15:15:03 -0500

    deps

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,6 +1,9 @@
 module.exports = ({ config }) => {
-    return {
-      ...config,
-      
-    }
-  };
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      package: 'com.ninemediacreative.cleaningboss',
+    },
+  }
+}
