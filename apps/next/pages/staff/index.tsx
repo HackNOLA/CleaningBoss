04a0688016887300af6e39aa5315ce68e402d296
asp2131 <akinpds@gmail.com>
2023-11-08 14:15:16 -0600

    styling-fix

--- apps/next/pages/staff/index.tsx
+++ apps/next/pages/staff/index.tsx
@@ -63,48 +63,53 @@ const StaffPage = () => {
 
   return (
     <YStack paddingTop={200}>
-      <XStack
-        space="$3"
-        alignItems="center"
-        borderColor={'slategray'}
-        borderWidth={1}
-        borderRadius={5}
-        width={350}
-        height={50}
-        padding={8}
-        style={{ position: 'sticky' }}
+      <YStack
+        // justifyContent="center"
+        // alignItems="center"
+        style={{ position: 'fixed', right: 20, left: 20, top: 140 }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="24"
-          height="24"
-          viewBox="0 0 50 50"
+        <XStack
+          space="$3"
+          alignItems="center"
+          borderColor={'slategray'}
+          borderWidth={1}
+          borderRadius={5}
+          width={350}
+          height={50}
+          padding={8}
         >
-          <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
-        </svg>
-        <input
-          value={searchTerm}
-          // borderColor={'transparent'}
-          placeholder="Search User"
-          onChange={handleSearchChange}
-        />
-      </XStack>
-      <XStack justifyContent="space-between" alignItems="baseline">
-        <XStack paddingTop={40} space="$2" justifyContent="center" alignItems="center">
-          <Button onPress={() => console.log('Filter users')} unstyled={true}>
-            {filterIcon}
-          </Button>
-          <Text fontSize={16} fontWeight="bold">
-            Filter
-          </Text>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="24"
+            height="24"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+          </svg>
+          <input
+            value={searchTerm}
+            // borderColor={'transparent'}
+            placeholder="Search User"
+            onChange={handleSearchChange}
+          />
         </XStack>
-        <Button onPress={() => console.log('Add user')} unstyled={true}>
-          {plusIcon}
-        </Button>
-      </XStack>
-      <YStack space="$4" paddingTop={40}>
+        <XStack justifyContent="space-between" alignItems="baseline">
+          <XStack paddingTop={40} space="$2" justifyContent="center" alignItems="center">
+            <Button onPress={() => console.log('Filter users')} unstyled={true}>
+              {filterIcon}
+            </Button>
+            <Text fontSize={16} fontWeight="bold">
+              Filter
+            </Text>
+          </XStack>
+          <Button onPress={() => console.log('Add user')} unstyled={true}>
+            {plusIcon}
+          </Button>
+        </XStack>
+      </YStack>
+      <YStack space="$4" top={40}>
         {filteredUsers.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
