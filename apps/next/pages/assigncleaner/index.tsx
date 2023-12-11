ad9074f2d39203e3aee3908b1cbd34403870b23f
unknown <360webexpert@gmail.com>
2023-12-11 13:21:58 +0530

    assigncleaner page added bottom nav

--- apps/next/pages/assigncleaner/index.tsx
+++ apps/next/pages/assigncleaner/index.tsx
@@ -10,6 +10,7 @@ import type { NextPage } from 'next'
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { Link } from 'solito/link'
 
 const supabase = createClient(
   'https://jqlnugxsnwftfvzsqfvv.supabase.co',
@@ -230,6 +231,8 @@ const AssignCleaner = () => {
 
   const router = useRouter()
 
+  const [page, setPage] = useState(0)
+
   useEffect(() => {
     // const fetchOrg = async () => {
     //   const { data } = await supabase.from('users').select().eq('id_company', org?.id)
@@ -331,6 +334,144 @@ const AssignCleaner = () => {
       </defs>
     </svg>
   )
+
+  function AdminBottomNav({ page, setPage }) {
+  return (
+    <>
+      <div
+        style={{ zIndex: 99 }}
+        className="fixed bottom-0 left-0 right-0 h-24 bg-white  shadow-lg flex items-center justify-around"
+      >
+        <Link href="#">
+          <div className="flex flex-col items-center justify-center text-center z-2">
+            <div
+              style={{ backgroundColor: page === 0 ? '#33CC4B' : 'transparent' }}
+              onClick={() => setPage(0)}
+              className={`w-10 h-10 rounded-full bg-[${
+                page === 0 ? '#33CC4B' : 'transparent'
+              }] flex items-center justify-center  z-4`}
+            >
+              <svg
+                className={` w-6 h-6 text-${page === 0 ? 'white' : 'black'}`}
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                <polyline points="9 22 9 12 15 12 15 22" />
+              </svg>
+            </div>
+            <span className="mt-1 text-xs">Dashboard</span>
+          </div>
+        </Link>
+        <Link href="#">
+          <div className="flex flex-col items-center justify-center text-center">
+            <div
+              onClick={() => setPage(1)}
+              style={{ backgroundColor: page === 1 ? '#33CC4B' : 'transparent' }}
+              className={`w-10 h-10 rounded-full bg-[${
+                page === 1 ? '#33CC4B' : 'transparent'
+              }] flex items-center justify-center`}
+            >
+              <svg
+                className={` w-6 h-6 text-${page === 1 ? 'white' : 'black'}`}
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect height="18" rx="2" ry="2" width="18" x="3" y="4" />
+                <line x1="16" x2="16" y1="2" y2="6" />
+                <line x1="8" x2="8" y1="2" y2="6" />
+                <line x1="3" x2="21" y1="10" y2="10" />
+              </svg>
+            </div>
+            <span className="mt-1 text-xs">Calendar</span>
+          </div>
+        </Link>
+        <Link href="#">
+          <div className="flex flex-col items-center justify-center text-center">
+            <div
+              onClick={() => setPage(2)}
+              style={{ backgroundColor: page === 2 ? '#33CC4B' : 'transparent' }}
+              className={`w-10 h-10 rounded-full bg-[${
+                page === 2 ? '#33CC4B' : 'transparent'
+              }] flex items-center justify-center`}
+            >
+              <svg
+                className={` w-6 h-6 text-${page === 2 ? 'white' : 'black'}`}
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect height="20" rx="2" ry="2" width="16" x="4" y="2" />
+                <path d="M9 22v-4h6v4" />
+                <path d="M8 6h.01" />
+                <path d="M16 6h.01" />
+                <path d="M12 6h.01" />
+                <path d="M12 10h.01" />
+                <path d="M12 14h.01" />
+                <path d="M16 10h.01" />
+                <path d="M16 14h.01" />
+                <path d="M8 10h.01" />
+                <path d="M8 14h.01" />
+              </svg>
+            </div>
+            <span className="mt-1 text-xs">Locations</span>
+          </div>
+        </Link>
+        <Link href="#">
+          <div className="flex flex-col items-center justify-center text-center">
+            <div
+              style={{ backgroundColor: page === 3 ? '#33CC4B' : 'transparent' }}
+              onClick={() => setPage(3)}
+              className={`w-10 h-10 rounded-full bg-[${
+                page === 3 ? '#33CC4B' : 'transparent'
+              }] flex items-center justify-center`}
+            >
+              <svg
+                className={` w-6 h-6 text-${page === 3 ? 'white' : 'black'}`}
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+              </svg>
+            </div>
+            <span className="mt-1 text-xs">Users</span>
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
+
   return (
     <YStack justifyContent="center" alignItems="center">
       <View width={'100%'}>{scrollPosition < 20 && <TopBar title="Assign Cleaners" />}</View>
@@ -390,10 +531,10 @@ const AssignCleaner = () => {
           </YStack>
         </div>
       )}
+      <AdminBottomNav page={page} setPage={setPage} />
         </YStack>
+
   )
 }
-
-
 
 export default AssignCleaner
