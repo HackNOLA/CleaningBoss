da80393f878b70e8491f132665d04567b7ff1858
asp2131 <akinpds@gmail.com>
2023-12-30 16:23:27 -0600

    cleaner dashboard done

--- apps/next/pages/cleaner/screen.tsx
+++ apps/next/pages/cleaner/screen.tsx
@@ -18,11 +18,21 @@ const supabase = createClient(
 )
 
 export default function Dashboard() {
-  const segments = ['Yesterday', 'Today', 'Tomorrow', 'Custom'] // Define your segments
+  const segments = ['Yesterday', 'Today', 'Tomorrow', 'Week'] // Define your segments
 
   const [selectedSegment, setSelectedSegment] = useState(0)
 
   const [locations, setLocations] = useState([])
+
+  const [jobs, setJobs] = useState([])
+
+  const [filteredJobs, setFilteredJobs] = useState([])
+
+  const [completedJobs, setCompletedJobs] = useState(0)
+
+  const [hoursWorked, setHoursWorked] = useState(0)
+
+  const [futureJobs, setFutureJobs] = useState([])
 
   const { email, activeUser, setActiveUser, setClerkId } = useContext(UserContext)
   const { orgName, setOrg } = useContext(OrgContext)
@@ -31,6 +41,45 @@ export default function Dashboard() {
 
   const handleSegmentChange = (index) => {
     setSelectedSegment(index)
+
+    const filteredJobs = jobs.filter((job) => {
+      const today = new Date()
+      const yesterday = new Date(today)
+      yesterday.setDate(yesterday.getDate() - 1)
+      const tomorrow = new Date(today)
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      const jobDate = new Date(job?.date)
+      if (index === 0) {
+        return jobDate.getDate() === yesterday.getDate()
+      }
+      if (index === 1) {
+        return jobDate.getDate() === today.getDate()
+      }
+      if (index === 2) {
+        return jobDate.getDate() === tomorrow.getDate()
+      }
+      if (index === 3) {
+        //if jobDate is between today and 7 days from now
+        return jobDate.getDate() >= today.getDate() && jobDate.getDate() <= tomorrow.getDate()
+      }
+    })
+    setFilteredJobs(filteredJobs)
+
+    const completedJobs = filteredJobs.filter((job) => job?.completed === true)
+    setCompletedJobs(completedJobs.length)
+
+    const hoursWorked = filteredJobs.reduce((acc, job) => {
+      const timeObj1 = new Date('2000-01-01 ' + job.clock_in_time)
+      const timeObj2 = new Date('2000-01-01 ' + job.clock_out_time)
+
+      // Calculate the time difference in milliseconds
+      const timeDifferenceMillis = timeObj2 - timeObj1
+      // Convert milliseconds to hours, minutes, and seconds
+      const hours = Math.floor(timeDifferenceMillis / (1000 * 60 * 60))
+      console.log(hours)
+      return acc + hours
+    }, 0)
+    setHoursWorked(hoursWorked)
   }
   useEffect(() => {
     deleteCookie('activeUser')
@@ -52,9 +101,21 @@ export default function Dashboard() {
       return
     }
     getUserInfo()
-  }, [activeUser])
+
+    // get future jobs
+    const today = new Date()
+    // if there are any jobs after today, set them to future jobs
+    const futureJobs = jobs.filter((job) => {
+      const jobDate = new Date(job?.date)
+      return jobDate.getDate() >= today.getDate()
+    })
+    setFutureJobs(futureJobs)
+  }, [activeUser, filteredJobs])
 
   const checkOrg = async (activeUser) => {
+    const { data: foundJobs } = await supabase.from('jobs').select().eq('id_user', activeUser.id)
+    setJobs(foundJobs)
+
     if (activeUser?.id_company) {
       const { data: foundOrg } = await supabase
         .from('company')
@@ -99,20 +160,64 @@ export default function Dashboard() {
         <Toast />
         {/* <CurrentToast bgColor={'green'} /> */}
         <View justifyContent="center" alignItems="center">
-          <Map />
+          <Map locations={locations} />
         </View>
         <View height={15} />
         <SegmentedControl segments={segments} onChange={handleSegmentChange} />
         <View height={15} />
         <YStack space="$4">
-          <XStack space="$4">
-            <Card title="Staffed" icon="users" info="224 of 338 hours scheduled" />
-            <Card title="Completed" icon="check-circle" info="4 out of 6 jobs completed" />
-          </XStack>
-          <XStack space="$4">
-            <Card title="Hours" icon="clock" info="224h worked out of 338h scheduled" />
-            <Card title="Cleaners" icon="user" info="8/10 Cleaners were active" />
-          </XStack>
+          {selectedSegment !== 3 && (
+            <>
+              <XStack space="$4">
+                <Card
+                  title="Completed Shifts"
+                  icon="users"
+                  info={`You completed ${
+                    filteredJobs.filter((job) => job.completed).length
+                  } jobs this day`}
+                />
+                <Card
+                  title="Future Shifts"
+                  icon="check-circle"
+                  info={`You have future ${futureJobs.length} jobs`}
+                />
+              </XStack>
+              <XStack space="$4" paddingTop={12}>
+                <Card
+                  title="Hours Worked"
+                  icon="clock"
+                  info={`You clocked in ${hoursWorked} hours this day`}
+                />
+                {/* <Card title="Cleaners" icon="user" info="8/10 Cleaners were active" /> */}
+              </XStack>
+            </>
+          )}
+          {selectedSegment === 3 && (
+            <>
+              <XStack space="$4">
+                <Card
+                  title="Completed Shifts"
+                  icon="users"
+                  info={`You completed ${
+                    filteredJobs.filter((job) => job.completed).length
+                  } job this week`}
+                />
+                <Card
+                  title="Future Shifts"
+                  icon="check-circle"
+                  info={`You have future ${futureJobs.length} jobs`}
+                />
+              </XStack>
+              <XStack space="$4" paddingTop={12}>
+                <Card
+                  title="Hours Worked"
+                  icon="clock"
+                  info={`You clocked in ${hoursWorked} hours this week`}
+                />
+                {/* <Card title="Cleaners" icon="user" info="8/10 Cleaners were active" /> */}
+              </XStack>
+            </>
+          )}
         </YStack>
       </YStack>
     </View>
