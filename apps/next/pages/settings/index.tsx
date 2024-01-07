ebc924710b1ef38262741e3c62efad93d82206a2
asp2131 <akinpds@gmail.com>
2024-01-07 09:44:01 -0600

    replaced rotue after signout

--- apps/next/pages/settings/index.tsx
+++ apps/next/pages/settings/index.tsx
@@ -21,7 +21,7 @@ export default function Signout() {
           <Button
             onPress={async () => {
               await signOut()
-              router.replace('/')
+              router.replace('/signin')
             }}
           >
             Sign Out
